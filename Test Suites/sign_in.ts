b7f7537d4dc1a84ed06a9b0442789fee92edd479
achraf bouchouik <abdoliloaf@gmail.com>
2022-10-27 19:34:55 +0100

    test sample

--- Test Suites/sign_in.ts
+++ Test Suites/sign_in.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>5b01c788-737f-4e7f-bc7c-96e679dbacae</testSuiteGuid>
+   <testCaseLink>
+      <guid>f9dca7b2-652a-47ab-bb58-8f81346b5d5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SignIn</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
