c3c2b181fbc704a05570aca3017a4903a93f47d7
achraf bouchouik <abdoliloaf@gmail.com>
2022-10-27 19:34:17 +0100

    test sample

---
+++ Test Suites/sign_in.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>sign_in</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5b01c788-737f-4e7f-bc7c-96e679dbacae</testSuiteGuid>
+</TestSuiteEntity>
