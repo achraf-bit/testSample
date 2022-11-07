ba18e30d539baba86cf0dd9f6fa3345e35993d94
achraf bouchouik <abdoliloaf@gmail.com>
2022-11-07 15:06:54 +0100

    test

--- Test Suites/sign_in.ts
+++ Test Suites/sign_in.ts
@@ -12,10 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>5b01c788-737f-4e7f-bc7c-96e679dbacae</testSuiteGuid>
    <testCaseLink>
-      <guid>f9dca7b2-652a-47ab-bb58-8f81346b5d5f</guid>
+      <guid>7d5aafb4-06a3-466b-983b-d57e9971f775</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/SignIn</testCaseId>
+      <testCaseId>Test Cases/login</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
