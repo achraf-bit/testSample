cf405ce70f1854b0140671294698ad7db2058f2b
achraf bouchouik <abdoliloaf@gmail.com>
2023-01-30 16:28:56 +0100

    tests

--- Test Suites/Tests CPB.ts
+++ Test Suites/Tests CPB.ts
@@ -12,17 +12,17 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>bf7bd890-7eae-44b6-97ae-a38bf1690942</testSuiteGuid>
    <testCaseLink>
-      <guid>d040171b-e196-45d6-b3c6-3bf2d2e57a2c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Order/Access To Order</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>73501a4b-a9a7-4a97-92d6-07581e7433fe</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Delivery/Access To Delivery</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d040171b-e196-45d6-b3c6-3bf2d2e57a2c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Order/Access To Order</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
